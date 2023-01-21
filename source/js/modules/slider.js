b01fa57103ee38c6e5f2a9961b7a7b40138abbb6
Igor Rebrishchev <igorr28@yandex.ru>
2023-01-21 23:22:42 +0300

    стилизация coaches

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,3 +1,36 @@
 import '../vendor/swiper.js';
 
 console.log('slider');
+
+const slider = document.querySelector('.swiper');
+console.log(slider);
+
+let mySwiper = new Swiper(slider, {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // loop: true,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      initialSlide: 2,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      initialSlide: 0,
+    },
+  },
+})
